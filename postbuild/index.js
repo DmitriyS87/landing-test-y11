d06a42fcb8b11f4ed1cbc4e2708ae9f1716133d9
Dmitriy Schukin <schukindmitriy@gmail.com>
2024-09-17 19:57:14 +0200

    fixed build

--- postbuild/index.js
+++ postbuild/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cssPath = path.join(__dirname, '../dist/styles/index.css');
 let css = fs.readFileSync(cssPath, 'utf8');
 
-// Adjust font paths
-css = css.replace(/url\(\.\.\/\.\.\/fonts\//g, "url(../fonts/");
+// Adjust font paths\
+css = css.replace(/url\(\.\.\/\.\.\//g, "url(../");
 
 fs.writeFileSync(cssPath, css);
